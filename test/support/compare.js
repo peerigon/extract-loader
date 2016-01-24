336eb579bb528801747ed5f1cca738496cb4337f
Johannes Ewald <johannes.ewald@peerigon.com>
2016-01-24 17:37:29 +0100

    Remove compare.js

--- test/support/compare.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import hashFile from "hash-file";
-
-export default function (testModule) {
-    const hash1 = hashFile.sync(path.resolve(__dirname, "../modules/", `${testModule}.js`));
-    const hash2 = hashFile.sync(path.resolve(__dirname, "../dist/", `${testModule}.js`));
-
-    return hash1 === hash2;
-}
