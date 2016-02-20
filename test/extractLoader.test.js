16cc3c3b6288a829e2884768dfe39937f9182131
Johannes Ewald <johannes.ewald@peerigon.com>
2016-02-20 16:06:39 +0100

    Simplify syntax in extractLoader.test.js

--- test/extractLoader.test.js
+++ test/extractLoader.test.js
@@ -11,34 +11,34 @@ describe("extractLoader", () => {
     afterEach(() => {
         rimRaf.sync(path.resolve(__dirname, "dist"));
     });
-    it("should extract 'hello' into simple.js", () => {
-        return compile({ testModule: "simple.js" }).then(() => {
+    it("should extract 'hello' into simple.js", () =>
+        compile({ testModule: "simple.js" }).then(() => {
             const simpleJs = path.resolve(__dirname, "dist/simple-dist.js");
 
             expect(simpleJs).to.be.a.file();
             expect(simpleJs).to.have.content("hello");
-        });
-    });
-    it("should extract the html of modules/simple.html into simple.html", () => {
-        return compile({ testModule: "simple.html" }).then(() => {
+        })
+    );
+    it("should extract the html of modules/simple.html into simple.html", () =>
+        compile({ testModule: "simple.html" }).then(() => {
             const simpleHtml = path.resolve(__dirname, "dist/simple-dist.html");
 
             expect(simpleHtml).to.be.a.file();
             expect(simpleHtml).to.have.content(
                 fs.readFileSync(path.resolve(__dirname, "modules/simple.html"), "utf8")
             );
-        });
-    });
-    it("should extract the css of modules/simple.css into simple.css", () => {
-        return compile({ testModule: "simple.css" }).then(() => {
+        })
+    );
+    it("should extract the css of modules/simple.css into simple.css", () =>
+        compile({ testModule: "simple.css" }).then(() => {
             const simpleCss = path.resolve(__dirname, "dist/simple-dist.css");
 
             expect(simpleCss).to.be.a.file();
             expect(simpleCss).to.have.content(
                 fs.readFileSync(path.resolve(__dirname, "modules/simple.css"), "utf8")
             );
-        });
-    });
+        })
+    );
     it("should extract the img url into img.js", () => {
         return compile({ testModule: "img.js" }).then(() => {
             const imgJs = path.resolve(__dirname, "dist/img-dist.js");
@@ -47,28 +47,28 @@ describe("extractLoader", () => {
             expect(imgJs).to.have.content("hi-dist.jpg");
         });
     });
-    it("should extract the img.html as file, emit the referenced img and rewrite the url", () => {
-        return compile({ testModule: "img.html" }).then(() => {
+    it("should extract the img.html as file, emit the referenced img and rewrite the url", () =>
+        compile({ testModule: "img.html" }).then(() => {
             const imgHtml = path.resolve(__dirname, "dist/img-dist.html");
             const imgJpg = path.resolve(__dirname, "dist/hi-dist.jpg");
 
             expect(imgHtml).to.be.a.file();
             expect(imgJpg).to.be.a.file();
             expect(imgHtml).to.have.content.that.match(/<img src="hi-dist\.jpg">/);
-        });
-    });
-    it("should extract the img.css as file, emit the referenced img and rewrite the url", () => {
-        return compile({ testModule: "img.css" }).then(() => {
+        })
+    );
+    it("should extract the img.css as file, emit the referenced img and rewrite the url", () =>
+        compile({ testModule: "img.css" }).then(() => {
             const imgCss = path.resolve(__dirname, "dist/img-dist.css");
             const imgJpg = path.resolve(__dirname, "dist/hi-dist.jpg");
 
             expect(imgCss).to.be.a.file();
             expect(imgJpg).to.be.a.file();
             expect(imgCss).to.have.content.that.match(/ url\(hi-dist\.jpg\);/);
-        });
-    });
-    it("should extract the stylesheet.html and the referenced img.css as file, emit the files and rewrite all urls", () => {
-        return compile({ testModule: "stylesheet.html" }).then(() => {
+        })
+    );
+    it("should extract the stylesheet.html and the referenced img.css as file, emit the files and rewrite all urls", () =>
+        compile({ testModule: "stylesheet.html" }).then(() => {
             const stylesheetHtml = path.resolve(__dirname, "dist/stylesheet-dist.html");
             const imgCss = path.resolve(__dirname, "dist/img-dist.css");
             const imgJpg = path.resolve(__dirname, "dist/hi-dist.jpg");
@@ -79,10 +79,10 @@ describe("extractLoader", () => {
             expect(stylesheetHtml).to.have.content.that.match(/<link href="img-dist\.css"/);
             expect(stylesheetHtml).to.have.content.that.match(/<img src="hi-dist\.jpg">/);
             expect(imgCss).to.have.content.that.match(/ url\(hi-dist\.jpg\);/);
-        });
-    });
-    it("should track all dependencies", () => {
-        return compile({ testModule: "stylesheet.html" }).then((stats) => {
+        })
+    );
+    it("should track all dependencies", () =>
+        compile({ testModule: "stylesheet.html" }).then((stats) => {
             const basePath = path.dirname(__dirname); // returns the parent dirname
             const dependencies = stats.compilation.fileDependencies.map(
                 (dependency) => dependency.slice(basePath.length)
@@ -94,24 +94,24 @@ describe("extractLoader", () => {
                 "/test/modules/img.css",
                 "/test/modules/stylesheet.html"
             ].sort());
-        });
-    });
-    it("should reference the img with the given publicPath", () => {
-        return compile({ testModule: "img.html", publicPath: "/test/" }).then(() => {
+        })
+    );
+    it("should reference the img with the given publicPath", () =>
+        compile({ testModule: "img.html", publicPath: "/test/" }).then(() => {
             const imgHtml = path.resolve(__dirname, "dist/img-dist.html");
             const imgJpg = path.resolve(__dirname, "dist/hi-dist.jpg");
 
             expect(imgHtml).to.be.a.file();
             expect(imgJpg).to.be.a.file();
             expect(imgHtml).to.have.content.that.match(/<img src="\/test\/hi-dist\.jpg">/);
-        });
-    });
-    it("should report syntax errors", () => {
-        return compile({ testModule: "error.js" }).then(
+        })
+    );
+    it("should report syntax errors", () =>
+        compile({ testModule: "error.js" }).then(
             () => { throw new Error("Did not throw expected error"); },
             ({ message }) => {
                 expect(message).to.match(/SyntaxError: Unexpected identifier/);
             }
-        );
-    });
+        )
+    );
 });
